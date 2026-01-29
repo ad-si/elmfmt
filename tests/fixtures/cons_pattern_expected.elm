5cc0545c9ee294c85040062d330b317d253e07c2
Adrian Sieber <mail@adriansieber.com>
2026-01-29 11:51:17 +0000

    Add spacing around :: operator in cons patterns

---
+++ tests/fixtures/cons_pattern_expected.elm
@@ -0,0 +1,28 @@
+module ConsPattern exposing (main)
+
+
+extractFirst list =
+    case list of
+        x :: xs ->
+            Just x
+
+        [] ->
+            Nothing
+
+
+extractSubmatch match =
+    case match.submatches of
+        (Just submatch) :: _ ->
+            submatch
+
+        _ ->
+            ""
+
+
+nestedCons list =
+    case list of
+        a :: b :: c :: rest ->
+            a + b + c
+
+        _ ->
+            0
