8468f34d92da4cb13ab9f731c4eb630678207564
Adrian Sieber <mail@adriansieber.com>
2026-01-29 06:59:57 +0000

    Fix expected test output

--- tests/fixtures/tuple_expected.elm
+++ tests/fixtures/tuple_expected.elm
@@ -1,7 +1,9 @@
 module Main exposing (multiTuple)
 
 multiTuple =
-  (newModel, Cmd.batch
-    [ setStorage base85Model
-    , cmd
-    ])
+  ( newModel
+  , Cmd.batch
+      [ setStorage base85Model
+      , cmd
+      ]
+  )
